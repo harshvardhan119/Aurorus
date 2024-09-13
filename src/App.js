f16f2f46f4a6b1386205564c5cbb852d73c8f28c
Harshvardhansingh19 <yobrobiru@gmail.com>
2024-09-13 08:33:13 +0530

    resolved

--- src/App.js
+++ src/App.js
@@ -7,14 +7,13 @@ import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Bar from "./scenes/bar";
-import Form from "./scenes/form";
+
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
+
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import Calendar from "./scenes/calendar/calendar";
 
 import Lineabc from "./scenes/State";
 import SearchStateForm from "./components/Prediction";
--- src/scenes/State/index.js
+++ src/scenes/State/index.js
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
 
-import LineChartWithDropdown from "../../components/States";
+
 
 import ChartsWithDropdown from "../../components/Statesbar";
 
