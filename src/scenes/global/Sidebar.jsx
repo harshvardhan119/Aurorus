c89eed87808e07bfd522930712f40c87f21059e3
Harshvardhansingh19 <yobrobiru@gmail.com>
2024-09-16 09:18:06 +0530

    Responsiveness Updated

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,218 +1,4 @@
-// import { useState } from "react";
-// import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
-// import { Box, IconButton, Typography, useTheme } from "@mui/material";
-// import { Link } from "react-router-dom";
-// import "react-pro-sidebar/dist/css/styles.css";
-// import { tokens } from "../../theme";
-// import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 
-// import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
-// import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
-// import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
-// import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
-// import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-// import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
-
-// const Item = ({ title, to, icon, selected, setSelected }) => {
-//   const theme = useTheme();
-//   const colors = tokens(theme.palette.mode);
-//   return (
-//     <MenuItem
-//       active={selected === title}
-//       style={{
-//         color: colors.grey[100],
-//       }}
-//       onClick={() => setSelected(title)}
-//       icon={icon}
-//     >
-//       <Typography>{title}</Typography>
-//       <Link to={to} />
-//     </MenuItem>
-//   );
-// };
-
-// const Sidebar = () => {
-//   const theme = useTheme();
-//   const colors = tokens(theme.palette.mode);
-//   const [isCollapsed, setIsCollapsed] = useState(false);
-//   const [selected, setSelected] = useState("Dashboard");
-
-//   return (
-//     <Box
-//       sx={{position:"fixed",
-//             height: "100vh",
-//         "& .pro-sidebar-inner": {
-//           background: `${colors.primary[400]} !important`,
-//         },
-//         "& .pro-icon-wrapper": {
-//           backgroundColor: "transparent !important",
-//         },
-//         "& .pro-inner-item": {
-//           padding: "5px 35px 5px 20px !important",
-//         },
-//         "& .pro-inner-item:hover": {
-//           color: "#868dfb !important",
-//         },
-//         "& .pro-menu-item.active": {
-//           color: "#6870fa !important",
-//         },
-//       }}
-//     >
-//       <ProSidebar collapsed={isCollapsed}>
-//         <Menu iconShape="square">
-//           {/* LOGO AND MENU ICON */}
-//           <MenuItem
-//             onClick={() => setIsCollapsed(!isCollapsed)}
-//             icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
-//             style={{
-//               margin: "10px 0 20px 0",
-//               color: colors.grey[100],
-//             }}
-//           >
-//             {!isCollapsed && (
-//               <Box
-//                 display="flex"
-//                 justifyContent="space-between"
-//                 alignItems="center"
-//                 ml="15px"
-//               >
-//                 <Typography variant="h3" color={colors.grey[100]}>
-//                   Aurora
-//                 </Typography>
-//                 <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
-//                   <MenuOutlinedIcon />
-//                 </IconButton>
-//               </Box>
-//             )}
-//           </MenuItem>
-
-//           {!isCollapsed && (
-//             <Box mb="25px" >
-              
-//               <Box textAlign="center">
-//                 <Typography
-//                   variant="a"
-//                   color={colors.grey[100]}
-//                   fontWeight="bold"
-//                   sx={{ m: "10px 0 0 0" }}
-//                 >
-//                   Water is the driving force of all nature.
-//                 </Typography>
-//                 <Typography variant="h5" color={colors.greenAccent[500]}>
-//                 Forecasting water resources today ensures a sustainable tomorrow.
-//                 </Typography>
-//               </Box>
-//             </Box>
-//           )}
-
-//           <Box paddingLeft={isCollapsed ? undefined : "10%"}>
-//           <Item
-//               title="Prediction Model"
-//               to="/model"
-//               icon={<HomeOutlinedIcon />}
-//               selected={selected}
-//               setSelected={setSelected}
-//             />
-//             <Item
-//               title="India's Detail"
-//               to="/"
-//               icon={<HomeOutlinedIcon />}
-//               selected={selected}
-//               setSelected={setSelected}
-//             />
-//             <Typography
-//               variant="h6"
-//               color={colors.grey[300]}
-//               sx={{ m: "15px 0 5px 20px" }}
-//             >
-//               Charts
-//             </Typography>
-//             <Item
-//               title="State Chart"
-//               to="/bar"
-//               icon={<BarChartOutlinedIcon />}
-//               selected={selected}
-//               setSelected={setSelected}
-//             />
-//             <Item
-//               title="Region Chart"
-//               to="/pie"
-//               icon={<PieChartOutlineOutlinedIcon />}
-//               selected={selected}
-//               setSelected={setSelected}
-//             />
-//             <Item
-//               title="Yearly Water Consumption"
-//               to="/line"
-//               icon={<TimelineOutlinedIcon />}
-//               selected={selected}
-//               setSelected={setSelected}
-//             />
-//             <Item
-//               title="State wise Chart"
-//               to="/geography"
-//               icon={<MapOutlinedIcon />}
-//               selected={selected}
-//               setSelected={setSelected}
-//             />
-//              <Typography
-//               variant="h6"
-//               color={colors.grey[300]}
-//               sx={{ m: "15px 0 5px 20px" }}
-//             >
-//               Prediction Model</Typography>
-//               <Item
-//               title="Forecast Water"
-//               to="/calendar"
-//               icon={<HelpOutlineOutlinedIcon />}
-//               selected={selected}
-//               setSelected={setSelected}
-//             />
-//              <Item
-//               title="predict Requirement"
-//               to="/form"
-//               icon={<HelpOutlineOutlinedIcon />}
-//               selected={selected}
-//               setSelected={setSelected}
-//             />
-//             <Typography
-//               variant="h6"
-//               color={colors.grey[300]}
-//               sx={{ m: "15px 0 5px 20px" }}
-//             >
-//               Reservoir Management</Typography>
-//               <Item
-//               title="Choropleth Map"
-//               to="/Reservoirchloropleth"
-//               icon={<HelpOutlineOutlinedIcon />}
-//               selected={selected}
-//               setSelected={setSelected}
-//             />
-             
-//             <Typography
-//               variant="h6"
-//               color={colors.grey[300]}
-//               sx={{ m: "15px 0 5px 20px" }}
-//             >
-//               Refrence
-//             </Typography>
-            
-            
-//             <Item
-//               title="Source"
-//               to="/faq"
-//               icon={<HelpOutlineOutlinedIcon />}
-//               selected={selected}
-//               setSelected={setSelected}
-//             />
-//           </Box>
-//         </Menu>
-//       </ProSidebar>
-//     </Box>
-//   );
-// };
-
-// export default Sidebar;
 import { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Box, IconButton, Typography, useTheme, useMediaQuery } from "@mui/material";
@@ -226,6 +12,9 @@ import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutl
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import { PiMapPinLineThin } from "react-icons/pi";
+import { GiDatabase } from "react-icons/gi";
+import { FaHouseFloodWater } from "react-icons/fa6";
 
 const Item = ({ title, to, icon, selected, setSelected }) => {
   const theme = useTheme();
@@ -321,7 +110,7 @@ const Sidebar = () => {
             <Item
               title="India's Detail"
               to="/"
-              icon={<HomeOutlinedIcon />}
+              icon={<PiMapPinLineThin />}
               selected={selected}
               setSelected={setSelected}
             />
@@ -362,14 +151,14 @@ const Sidebar = () => {
             <Item
               title="Forecast Water"
               to="/calendar"
-              icon={<HelpOutlineOutlinedIcon />}
+              icon={<FaHouseFloodWater />}
               selected={selected}
               setSelected={setSelected}
             />
             <Item
               title="Predict Requirement"
               to="/form"
-              icon={<HelpOutlineOutlinedIcon />}
+              icon={<GiDatabase />}
               selected={selected}
               setSelected={setSelected}
             />
@@ -379,7 +168,7 @@ const Sidebar = () => {
             <Item
               title="Choropleth Map"
               to="/Reservoirchloropleth"
-              icon={<HelpOutlineOutlinedIcon />}
+              icon={<MapOutlinedIcon />}
               selected={selected}
               setSelected={setSelected}
             />
